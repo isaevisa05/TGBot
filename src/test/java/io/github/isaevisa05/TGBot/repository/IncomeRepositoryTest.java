9f0921832457649b0be6b20fce724e745e6e6ded
isaevisa05 <isaevisao55@gmail.com>
2025-10-28 00:53:02 +0300

    Update

--- src/test/java/io/github/isaevisa05/TGBot/repository/IncomeRepositoryTest.java
+++ src/test/java/io/github/isaevisa05/TGBot/repository/IncomeRepositoryTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 public class IncomeRepositoryTest {
@@ -21,5 +23,24 @@ public class IncomeRepositoryTest {
         final List<Income> found = incomeRepository.findAll();
         assertEquals(10, found.size());
     }
+
+    @Test
+    public void testDataScripts() {
+        var income = incomeRepository.findById(12345L);
+        System.out.println(income);
+
+        if(income.isEmpty()) System.out.println(true);
+        if(income.isEmpty()) System.out.println(true);
+        if(income.isEmpty()) System.out.println(true);
+        if(income.isEmpty()) System.out.println(true);
+        if(income.isEmpty()) System.out.println(true);
+        if(income.isEmpty()) System.out.println(true);
+        if(income.isEmpty()) System.out.println(true);
+        if(income.isEmpty()) System.out.println(true);
+
+        //assertTrue(income.isPresent());
+        //assertEquals(income.get().getIncome(), new BigDecimal("43500"));
+    }
+
 
 }
