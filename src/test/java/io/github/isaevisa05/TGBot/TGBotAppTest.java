aca3e9bfb684b4906ece7622a71210a56543bf14
isaevisa05 <isaevisao55@gmail.com>
2025-10-28 15:06:32 +0300

    Update

--- src/test/java/io/github/isaevisa05/TGBot/TGBotAppTest.java
+++ src/test/java/io/github/isaevisa05/TGBot/TGBotAppTest.java
@@ -1,13 +1,11 @@
 package io.github.isaevisa05.TGBot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 public class TGBotAppTest {
 
-    @Test
-    public void contextLoads() {
-
-    }
 }
